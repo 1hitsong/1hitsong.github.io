3677b0598359c95b72edab7debfaedb90f1f02cc
1hitsong <3330318+1hitsong@users.noreply.github.com>
2024-12-11 19:14:42 -0500

    Remove all references to google analytics

--- assets/js/data/swcache.js
+++ assets/js/data/swcache.js
@@ -43,11 +43,6 @@ const resource = [
 
 /* The request url with below domain will be cached */
 const allowedDomains = [
-  {% if site.google_analytics.id != '' %}
-    'www.googletagmanager.com',
-    'www.google-analytics.com',
-  {% endif %}
-
   '{{ site.url | split: "//" | last }}',
 
   'fonts.gstatic.com',
@@ -58,7 +53,4 @@ const allowedDomains = [
 
 /* Requests that include the following path will be banned */
 const denyUrls = [
-  {% if site.google_analytics.pv.cache_path %}
-    '{{ site.google_analytics.pv.cache_path | absolute_url }}'
-  {% endif %}
 ];
